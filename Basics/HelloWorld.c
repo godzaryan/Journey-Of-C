10f83e137a764f4d3c99c3b80bd9b5ddb116d322
Akash Kumar <cronoquillgamers2018@gmail.com>
2024-05-30 10:39:25 +0530

    Add files via upload

---
+++ Basics/HelloWorld.c
@@ -0,0 +1,20 @@
+//Including headers for using fuctions like printf(), scanf() etc...
+#include <stdio.h>
+
+//Main function (Entry point of the whole program)
+//consists of two parts :
+//"int" which is it's return type and
+//"main" which is the function name
+int main()
+{//beginning of the fuction
+
+   //printf() fuction from <stdio.h> header
+   //printf() fuction is used to print something in console as output
+   //if we breakdown printf() then we will get "print" which means to print/output/show something
+   //and "f" which meaning formatted, which means printf() is used to print output in formatted way
+   printf("Hello World");
+
+   //Returning a value after execution of main() function
+   return 0;
+
+}//end of function
